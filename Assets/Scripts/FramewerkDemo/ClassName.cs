5da5b203787ea48c7b1ebc6e519d861895200aa6
MatejOsanec <matejosanec@fb.com>
2021-10-10 22:14:05 +0200

    fix formatting

--- Assets/Scripts/FramewerkDemo/ClassName.cs
+++ Assets/Scripts/FramewerkDemo/ClassName.cs
@@ -3,45 +3,45 @@ using UnityEngine;
 
 sing UnityEngine;
 
-public class ClassName : Monobehaviour {
-
+public class ClassName : Monobehaviour
+{
     [SerializeField] SomeClass _reference = default;
-  
+
     [SerializeField] float _speed = 0.5f;
 
     [Inject] readonly OtherClass _otherReference = default;
 
-    public enum NestedEnum {
-    
+    public enum NestedEnum
+    {
         First,
         Second
     }
-  
-    public class NestedClass {
-  
+
+    public class NestedClass
+    {
         public int value;
     }
-  
+
     public NestedClass anotherReference => _anotherReference;
-  
+
     public float publicVariable;
-  
+
     public event System.Action somethingDidHappenEvent;
-  
+
     private bool somePrivateProperty => _anotherReference.value != kSomeConstant;
-  
+
     private NestedClass _anotherReference;
-  
+
     private const int kSomeConstant = 0;
-  
-  
-    protected void Awake() {
-  
+
+
+    protected void Awake()
+    {
         // Do Something
     }
-  
-    public void NonUnityMethod() {
-  
+
+    public void NonUnityMethod()
+    {
         // Do Something
     }
 }
