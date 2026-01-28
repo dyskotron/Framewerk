1beb90a5413919204f3aec392fb5a713d27f6287
hex-clawd <hex-clawd@users.noreply.github.com>
2026-01-28 22:19:36 +0100

    Fix scene: wire ViewConfig to Bootstrap, add null safety

--- Assets/Scripts/FramewerkDemo/FramewerkDemoBootstrap.cs
+++ Assets/Scripts/FramewerkDemo/FramewerkDemoBootstrap.cs
@@ -1,5 +1,6 @@
 using Plugins.Framewerk;
 using strange.extensions.context.impl;
+using UnityEngine;
 
 namespace FramewerkDemo
 {
@@ -11,13 +12,20 @@ namespace FramewerkDemo
 
         private void Start()
         {
+            if (viewConfig == null)
+            {
+                Debug.LogError("[FramewerkDemo] ViewConfig is not assigned on FramewerkDemoBootstrap! " +
+                               "Please assign a ViewConfig component in the inspector.", this);
+                return;
+            }
+
             _context = new FramewerkDemoContext(this, viewConfig);
             _context.Start();
         }
 
         private void OnApplicationQuit()
         {
-            _context.OnRemove();
+            _context?.OnRemove();
         }
     }
 }
