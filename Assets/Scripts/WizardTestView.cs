d6e7d8fada64023bd090bf02314c58542e1c803a
hex-clawd <hex-clawd@users.noreply.github.com>
2026-01-30 21:57:28 +0100

    Revert "Remove CC test wizard output"
    This reverts commit 0b35e0ea313b0e041517de0bfb6ec6f9733eb214.

---
+++ Assets/Scripts/WizardTestView.cs
@@ -0,0 +1,13 @@
+using Framewerk.UI.List;
+using UnityEngine;
+
+namespace Scripts
+{
+    public class WizardTestView : ListView
+    {
+        // Inherited from ListView:
+        // public RectTransform ContentsParent;
+        // public GameObject ItemPrefab;
+        // public GameObject EmptyContent;
+    }
+}
